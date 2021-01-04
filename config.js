7eaa8877f5f8e4e8b7f4146ff543c9f284daae00
Muhammad Rizqi <muhammad-rizqi@live.com>
2021-01-04 09:55:57 +0700

    init project

---
+++ config.js
@@ -0,0 +1,10 @@
+const dotenv = require("dotenv").config();
+
+module.exports = {
+  NODE_ENV: process.env.NODE_ENV || "development",
+  HOST: process.env.HOST || "127.0.0.1",
+  PORT: process.env.PORT || 3000,
+  DB_NAME: process.env.DB_NAME,
+  DB_PASS: process.env.DB_PASS,
+  JWT_KEY: process.env.JWT_KEY,
+};
