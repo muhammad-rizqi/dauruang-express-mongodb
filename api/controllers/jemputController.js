251f0660c5ff2b85de7f8416e1a4e99d8ebf5e9c
Muhammad Rizqi <muhammad-rizqi@live.com>
2021-01-19 14:15:47 +0700

    fix typo

--- api/controllers/jemputController.js
+++ api/controllers/jemputController.js
@@ -38,7 +38,7 @@ exports.get_jemput = async (req, res) => {
         const pengurus = jemput.pengurus
           ? {
               id: jemput.pengurus._id,
-              nama_lengkap: jemput.pengurus.nana_lengkap,
+              nama_lengkap: jemput.pengurus.nama_lengkap,
               telepon: jemput.pengurus.telepon,
               lokasi: jemput.pengurus.lokasi,
               avatar: jemput.pengurus.avatar,
