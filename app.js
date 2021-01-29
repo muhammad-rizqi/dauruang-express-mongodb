c193d208627c0b8ef06e077344d28cf5264e06f7
Muhammad Rizqi <muhammad-rizqi@live.com>
2021-01-29 11:20:45 +0700

    update cors

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const mongoose = require("mongoose");
 const { DB_URL } = require("./config");
 const app = express();
 const checkAuth = require("./api/middleware/check-auth");
+const app = express();
 
+app.use(cors());
 // mongoose
 mongoose
   .connect(DB_URL, {
@@ -26,21 +28,6 @@ app.use(morgan("dev"));
 app.use("/uploads", express.static("uploads"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// CORS
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.WEB);
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
-});
 
 // routes
 const authRoutes = require("./api/routes/auth");
